3e9be2d61f82f7ae5e7e7c78bb231fbc8f85bcf2
Poglonesbr <131016905+Poglonesbr@users.noreply.github.com>
2024-05-15 14:05:04 -0300

    Checked

--- Aula 1/Ex3.cpp
+++ Aula 1/Ex3.cpp
@@ -1,52 +1,65 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
  class Retangulo{
      public:
-        Retangulo(){};
-        ~Retangulo(){};
+        Retangulo(){};          // Construtor
+        ~Retangulo(){};         // Destrutor
     
-        float calculeArea(){
-            area =  altura * largura;
-            return area;
+        float calculeArea(){                //Metodo para calcular a area
+            area =  altura * largura;       // Conta 
+            return area;                    // Retornar o valor da area para deixar salvo na Class
+        };
+            
+        float calculePerimetro(){                   //Metodo para calcular o perimetro
+            perimetro = 2 * (largura + altura);     // Conta 
+            return perimetro;                       // Retornar o valor do perimetro para deixar salvo na Class
         };
         
-        float calculePerimetro(){
-            perimetro = 2 * (largura + altura);
-            return perimetro;
-        };
-        
-        void setMedidas(float _altura, float _largura){
+        void setMedidas(float _altura, float _largura){     //Metodo para salvar o valor na Class e não pedir como parametros nos metodos de calcular
             altura = _altura;
             largura = _largura;
         };
 
-        };
+        float getPerimetro(){               // Metodo que utiliza o valor da class caso tenha uma alteração
+            return perimetro;
+            
+        }
         
-    private:
+        float getArea(){                    // Metodo que utiliza o valor da class caso tenha uma alteração
+            return area;
+        }
+
+        
+    private:                              //Declaração de variaveis na Class
         float altura;
         float largura;
         float area;
         float perimetro;
 };
 
-int main() {
-        Retangulo conta;
-        float altura;
+int main() {    
+        Retangulo conta;        //Instânciar o objeto Retangulo no main
+        float altura;           //Declaração de variaveis no main
         float largura;
         float area;
         float perimetro;
         
-        std::cout << "Digite o valor da altura: ";
+        std::cout << "Digite o valor da altura: ";      //Pedir os valores para realizar as contas
         std::cin >> altura;
         std::cout << "Digite o valor da largura: ";
         std::cin >> largura;
         
-        conta.setMedidas(altura, largura);
-        area = conta.calculeArea();
-        std::cout << "A área é: " << area;
-        std::cout << "\n";
-        perimetro = conta.calculePerimetro();
-        std::cout << "O perimetro é " << perimetro;
+        conta.setMedidas(altura, largura);          //Mandar os valores das medidas para a class Retangulo
+        
+        conta.calculeArea();                        // Chamar o metodo para realizar as contas
+        conta.calculePerimetro();
+        
+        
+        std::cout << "A área é: " << conta.getArea() << endl;               // Printar os valores na tela *endl é como o \n
+        std::cout << "O perimetro é " << conta.getPerimetro() << endl;
 
     return 0;
     }
