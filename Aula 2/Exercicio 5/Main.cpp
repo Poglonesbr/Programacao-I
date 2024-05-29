902abfb44bdf48f15f6bfd808c55b3555340b13f
Henryzzin <131017175+Henryzzin@users.noreply.github.com>
2024-05-29 14:27:54 +0000

    class livros feita

--- Aula 2/Exercicio 5/Main.cpp
+++ Aula 2/Exercicio 5/Main.cpp
@@ -1,15 +1,44 @@
 #include <iostream>
 #include <string>
+#include <vector>
 using namespace std;
 
-
-class Funcionario {
+class Livro {
 public:
-    Funcionario() {};       // Construtor
-    ~Funcionario() {};
+    Livro() {};       // Construtor
+    ~Livro() {};
 
+    void setTitulo(std::string _titulo){
+        titulo=_titulo;
+    }
+    void setAutor(std::string _autor){
+        autor=_autor;
+    }
+    void setISBN(std::string _ISBN){
+        ISBN=_ISBN;
+    }
+    void setEditora(std::string _editora){
+        editora=_editora;
+    }
+    void setLoc(std::string _localizacao){
+        localizacao=_localizacao;
+    }
+    void setNumCopia(int _num_copia){
+        num_copia=_num_copia;
+    }
+    void setId(int _id){
+        id=_id;
+    }
+    void setIsEmprestado(bool _isEmprestado){
+        isEmprestado=_isEmprestado;
+    }
+    
+private:
+    std::string titulo, autor, ISBN, editora, localizacao;
+    int num_copia, id;
+    bool isEmprestado=false;
 };
 
 int main() {
-
+    vector<Livro> livros;
 }
