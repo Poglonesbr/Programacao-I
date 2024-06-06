d430034841149a9e585a0647d3decaafea89ad85
Poglones <pedrohenriquebdossantos9@gmail.com>
2024-06-06 14:37:53 -0300

    Incluí hpp

---
+++ Aula 2/Exercicio 5/Usuario.hpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Usuario {
+public:
+	Usuario() {};
+	~Usuario() {};
+
+	void setInfo(std::string _senha, std::string _email, std::string _nome) {
+
+	}
+
+	Usuario fazerLogin(std::string email, std::string senha, std::vector<Usuario>* usuarios) {
+
+	}
+
+	void pagarMulta() {
+		}
+
+	void consultarMulta() {
+		
+	}
+
+
+	void verLivros() {
+
+	}
+
+
+};
+
+
+void cadastro(std::vector<Usuario*> usuarios) {
+	
+}
