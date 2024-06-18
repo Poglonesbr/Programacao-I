0b6e5bd424448c787ee579a5eb49f90dc1e95426
Henryzzin <131017175+Henryzzin@users.noreply.github.com>
2024-06-18 14:03:09 +0000

    fiz o emprestimo

--- Aula 2/Exercicio 5/Emprestimo.cpp
+++ Aula 2/Exercicio 5/Emprestimo.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <ctime>
+#include <sstream>
+#include <windows.h>
+#include "Emprestimo.hpp"
 
+void Emprestimo::fazerEmprestimo(){
+    multa=0;
+    dtaEmprestimo= std::chrono::system_clock::now();
+    dtaDevolucao= dtaEmprestimo + std::chrono::hours(24 * 7 * 2);
+    std::cout << "Digite o ISBN do livro que será emprestado. ";
+    std::cin >> ISBN
+
+    for(int i=0; i< livros->size(); i++){
+        if (ISBN == livros->at(i).ISBN && livros.(i)isEmprestado == false){
+            livros.(i)isEmprestado = true;
+            return;
+        }
+    }
+    std::cout << "O código ISBN digitado foi inválido. Digite CORRETAMENTE.";
+    fazerEmprestimo();
+}
